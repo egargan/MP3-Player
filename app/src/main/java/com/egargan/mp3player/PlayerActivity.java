adbd88ad2cd97c27351e576df526a35c4998f691
Edward Gargan <edwgargan@gmail.com>
2017-11-07 11:25:09 +0000

    Initial commit of project files.

---
+++ app/src/main/java/com/egargan/mp3player/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.egargan.mp3player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
